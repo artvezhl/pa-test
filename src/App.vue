9f88e499aca4829cb309e3b267628a88a0950f21
artvezhl <artvezhl@gmail.com>
2022-08-03 19:38:49 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1 class="text-3xl font-bold underline">
+    Hello test!
+  </h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
 
